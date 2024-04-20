14245bf3154a460dbaf21fef7beb03771dc42757
Stephen Ting <stephenting22@gmail.com>
2024-04-19 19:58:05 -0700

    Updated version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='autodft',
-    version='0.0.1a',
+    version='0.0.2',
     author='Shenvi Lab',
     author_email='sting@scripps.edu',
     url='https://github.com/shenvilab/autodft',
